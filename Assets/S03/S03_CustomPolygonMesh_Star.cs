fe2c411903a2a5dd4ee80a38da9061f7dfa78b6e
Chaebin49 <2chaebin@ewha.ac.kr>
2026-09-11 23:05:18 +0900

    feat: S03 별 모양 정점 11개 좌표 정의

--- Assets/S03/S03_CustomPolygonMesh_Star.cs
+++ Assets/S03/S03_CustomPolygonMesh_Star.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_CustomPolygonMesh_Star : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f),    // 0 중심점
+            new Vector3(0f, 4f, 0f),    // 1 바깥
+            new Vector3(1f, 1f, 0f),    // 2 안쪽
+            new Vector3(4f, 1f, 0f),    // 3 바깥
+            new Vector3(2f, -1f, 0f),   // 4 안쪽
+            new Vector3(3f, -4f, 0f),   // 5 바깥
+            new Vector3(0f, -2f, 0f),   // 6 안쪽
+            new Vector3(-3f, -4f, 0f),  // 7 바깥
+            new Vector3(-2f, -1f, 0f),  // 8 안쪽
+            new Vector3(-4f, 1f, 0f),   // 9 바깥
+            new Vector3(-1f, 1f, 0f),   // 10 안쪽
+        };
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
